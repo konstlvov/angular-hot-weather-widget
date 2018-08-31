4b48cfca4ce4c93b9b721e6bc5b6da2ffc64cdc8
Konstantin Lvov <k.lvov@abr.ru>
2018-08-31 18:06:32 +0300

    Сделал байндинг свойств для компонента social-widget.component

--- src/app/social-widget/social-widget.component.ts
+++ src/app/social-widget/social-widget.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-social-widget',
   templateUrl: './social-widget.component.html',
   styleUrls: ['./social-widget.component.css']
 })
+
 export class SocialWidgetComponent implements OnInit {
 
+  @Input() public followers: number = 0;
+  @Input() public following: number = 0;
   constructor() { }
 
   ngOnInit() {
