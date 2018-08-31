2c9b51086e51c3608f0e8a6c76e54ad991eefcf2
Konstantin Lvov <k.lvov@abr.ru>
2018-08-31 17:53:06 +0300

    Сделал байндинг свойств родительского компонента к свойствам дочернего temperature-widget (сам байндинг в этой строке: [airTemp]="currentAirTemp" [waterTemp]="currentWaterTemp")

--- src/app/temperature-widget/temperature-widget.component.ts
+++ src/app/temperature-widget/temperature-widget.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-temperature-widget',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemperatureWidgetComponent implements OnInit {
 
+  @Input() public airTemp: string = '20';
+  @Input() public waterTemp: string = '14';
   constructor() { }
 
   ngOnInit() {
