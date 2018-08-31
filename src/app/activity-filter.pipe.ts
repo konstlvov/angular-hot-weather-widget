deaee50884edde24f4f6c17c4b192e80454fa2d4
Konstantin Lvov <k.lvov@abr.ru>
2018-08-31 17:32:36 +0300

    Сделал пайп, который фильтрует отели (активности) по типам при клике на заголовок

--- src/app/activity-filter.pipe.ts
+++ src/app/activity-filter.pipe.ts
@@ -5,8 +5,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ActivityFilterPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  // todo: надо определить тип для activities
+  transform(activities: any[], filterCriteria: string): any {
+    if (!filterCriteria) {
+      return activities;
+    }
+    return activities.filter(a => a.actType === filterCriteria);
   }
 
 }
