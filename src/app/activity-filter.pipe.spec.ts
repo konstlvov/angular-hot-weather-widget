411e20d296d477a2c1ea04ed2f332a7241caca4c
Konstantin Lvov <k.lvov@abr.ru>
2018-09-11 13:40:06 +0300

    Auto-generated commit

--- src/app/activity-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ActivityFilterPipe } from './activity-filter.pipe';
-
-describe('ActivityFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ActivityFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
