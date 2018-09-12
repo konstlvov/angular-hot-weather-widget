8e09ebb3e809621ff5381d35a0dabb791c2ff1fb
Konstantin Lvov <k.lvov@abr.ru>
2018-09-12 17:09:57 +0300

    Добавлена типизация: IAddress, IWeather, ISocialInfo

---
+++ types/activities/index.d.ts
@@ -0,0 +1,24 @@
+﻿interface IAddress {
+  address1: string;
+  address2: string;
+  phone: string;
+}
+
+interface IWeather {
+  airTemp: number;
+  waterTemp: number;
+}
+
+interface ISocialInfo {
+  followers: number;
+  following: number;
+}
+
+
+interface IActivity {
+  //{actType: 'Hotel', address1: 'Венеция', address2: 'Piazza San-Marco', phone: 'Tel:+1285 968 685 1', airTemp: '40', waterTemp: '20', followers: 100, following: 500}
+  actType: string;
+  address: IAddress;
+  weather: IWeather;
+  socialInfo: ISocialInfo;
+}
