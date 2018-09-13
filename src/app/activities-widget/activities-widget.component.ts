eff249042692b0a377853965ca94069ec3cf5f21
Konstantin Lvov <k.lvov@abr.ru>
2018-09-13 12:17:47 +0300

    Перенес список мест отдыха в компонент app-activities-widget

--- src/app/activities-widget/activities-widget.component.ts
+++ src/app/activities-widget/activities-widget.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {activitiesList$} from '../mocks/activities';
 
 @Component({
   selector: 'app-activities-widget',
   templateUrl: './activities-widget.component.html',
   styleUrls: ['./activities-widget.component.css']
 })
+
 export class ActivitiesWidgetComponent implements OnInit {
+  public currentActivity: IActivity;
+  public activitiesList$ = activitiesList$;
+  
+  public hotelClick(activity) {
+    this.currentActivity = activity;
+  }
+
+  public criteriaClicked(whatClicked) {
+    this.filterCriteria = whatClicked;
+    //console.log(`Filter criteria now is ${this.filterCriteria}`);
+  }
 
   constructor() { }
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {activitiesList$} from './mocks/activities';
 
 @Component({
   selector: 'app-root',
@@ -9,32 +8,6 @@ import {activitiesList$} from './mocks/activities';
 
 export class AppComponent implements OnInit {
   title = 'hww';
-  public activitiesList$ = activitiesList$;
-  public filterCriteria: string = '';
-  public currentAirTemp: string = '14';
-  public currentWaterTemp: string = '20';
-  public currentFollowers: number = 100;
-  public currentFollowing: number = 500;
-  public currentActivity: IActivity;
-  public hotelClick(activity) {
-    this.currentActivity = activity;
-    //console.log(activity.address1);
-    //this.currentAirTemp = activity.weather.airTemp;
-    //this.currentWaterTemp = activity.weather.waterTemp;
-    //this.currentFollowers = activity.socialInfo.followers;
-    //this.currentFollowing = activity.socialInfo.following;
-  }
-  public criteriaClicked(whatClicked) {
-    this.filterCriteria = whatClicked;
-    console.log(`Filter criteria now is ${this.filterCriteria}`);
-  }
   ngOnInit() {
-    //$(document).ready(function(c) {
-    //  $('.log-close').on('click', function(c){
-    //    $('.login-bottom').fadeOut('slow', function(c){
-    //        $('.login-bottom').remove();
-    //    });
-    //  });	  
-    //});
   }
 }
