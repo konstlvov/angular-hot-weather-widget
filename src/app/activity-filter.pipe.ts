0509790a817ddfcd15d929dacee7b85c60fd8a38
Konstantin Lvov <k.lvov@abr.ru>
2018-09-13 18:05:03 +0300

    в процессе доделок

--- src/app/activity-filter.pipe.ts
+++ src/app/activity-filter.pipe.ts
@@ -4,12 +4,18 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'activityFilter'
 })
 export class ActivityFilterPipe implements PipeTransform {
-
+  //@Output() public filterEE: EventEmitter<IActivity> = new EventEmitter();
   transform(activities: IActivity[], filterCriteria: string): IActivity[] {
     if (!filterCriteria || filterCriteria === 'All') {
       return activities;
     }
-    return activities.filter(a => a.actType === filterCriteria);
+    const filteredActivities: IActivity[] = activities.filter(a => a.actType === filterCriteria);
+    // todo: спросить, можно ли где-нибудь поймать event, если его выкидывать отсюда
+    // (то есть по событию срабатывания фильтрующего пайпа)
+    //if (filteredActivities.length) {
+    //  this.filterEE.emit(filteredActivities[0]);
+    //}
+    return filteredActivities;
   }
 
 }
