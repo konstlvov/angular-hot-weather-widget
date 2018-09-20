b63d224fc83909639d7ececacfe7fab1b89ad9c6
Konstantin Lvov <k.lvov@abr.ru>
2018-09-20 17:38:34 +0300

    Продолжаем конструировать live search: то, что ввел пользователь, прокидывается на сервер в параметре rq через HTTP POST

--- src/app/livesearch-widget/livesearch-widget.component.ts
+++ src/app/livesearch-widget/livesearch-widget.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, fromEvent } from 'rxjs';
-import { map, filter } from 'rxjs/operators';
+import { map, filter, switchMap } from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
 
 @Component({
   selector: 'app-livesearch-widget',
@@ -10,7 +11,7 @@ import { map, filter } from 'rxjs/operators';
 export class LivesearchWidgetComponent implements OnInit {
 
   public dbgMsg: string = '...';
-  public results: string = '***';
+  public results: any;
   public typeahead: Observable;
   constructor() { }
 
@@ -24,8 +25,12 @@ export class LivesearchWidgetComponent implements OnInit {
        map((e: KeyboardEvent) => e.target.value)
       ,filter((userInput: string) => userInput.length > 2)
       ,map(s => s.toUpperCase())
+      ,switchMap((userInput) => ajax({"url": "http://localhost:8090/api/endpoint", "method": "POST", "body": {"rq": userInput}}))
     );
-    this.typeahead.subscribe(data => this.dbgMsg = data)
+    this.typeahead.subscribe(data => {
+      this.results = data.response;
+      //this.dbgMsg = JSON.stringify(data);
+    });
   }
 
   ngOnInit() {
