098227c8ca135a5537537d7e04f57ccaf1ac9f0f
Konstantin Lvov <k.lvov@abr.ru>
2018-09-20 15:19:56 +0300

    Промежуточное: вместо навешивания через (input) реагируем на ввод в поле inpSearch с помощью создания Observable через fromEvent(inpSearch, 'input')

--- src/app/livesearch-widget/livesearch-widget.component.ts
+++ src/app/livesearch-widget/livesearch-widget.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, fromEvent } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-livesearch-widget',
@@ -9,13 +11,23 @@ export class LivesearchWidgetComponent implements OnInit {
 
   public dbgMsg: string = '...';
   public results: string = '***';
+  public typeahead: Observable;
   constructor() { }
 
   public onInput(e: KeyboardEvent) {
-    this.dbgMsg = 'You entered: ' + e.target.value;
+    //this.dbgMsg = 'You entered: ' + e.target.value; // works
+  }
+
+  public bindLiveSearch() {
+    const inpSearch: HTMLElement = document.getElementById('inpSearch');
+    this.typeahead = fromEvent(inpSearch, 'input').pipe(
+      map((e: KeyboardEvent) => e.target.value)
+    );
+    this.typeahead.subscribe(data => this.dbgMsg = data)
   }
 
   ngOnInit() {
+    this.bindLiveSearch();
   }
 
 }
