41ce8746c3b15c65113f2d7d7e0ccaabb70f39f8
Konstantin Lvov <k.lvov@abr.ru>
2018-09-24 16:03:56 +0300

    Сделал, чтобы увидело тип, возможно его надо было явно импортнуть в index.d.ts (?)

--- types/activities/index.d.ts
+++ types/activities/index.d.ts
@@ -22,3 +22,14 @@ interface IActivity {
   weather: IWeather;
   socialInfo: ISocialInfo;
 }
+
+
+interface IProduct {
+  _id: string;
+  title: string;
+  serial: number;
+  author: string;
+  price: number;
+  src: string;
+}
+
--- types/activities/products.d.ts
+++ types/activities/products.d.ts
@@ -1,9 +1,1 @@
-﻿interface IProduct {
-  _id: string;
-  title: string;
-  serial: number;
-  author: string;
-  price: number;
-  src: string;
-}
-
+﻿
