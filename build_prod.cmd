8ef34f7d1c7dc0daedf565b846de4e9c3ad638b5
Konstantin Lvov <k.lvov@abr.ru>
2019-04-22 11:57:19 +0300

    Инструкция по использованию скрипта сборки prod-версии

--- build_prod.cmd
+++ build_prod.cmd
@@ -1,1 +1,4 @@
+rem Запустить скрипт, когда отработает, содержимое dist/hww положить в корень ветки gh-pages данного репозитория
+rem и запушить.
+
 ng build --prod --base-href "https://konstlvov.github.io/angular-hot-weather-widget/"
